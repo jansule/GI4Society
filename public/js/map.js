6ee0e961b93979f85cabb287c6ab03488b43c076
jansule <j_sule02@wwu.de>
2017-11-27 12:52:30 +0100

    add contribution to map

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@
     var mymap = L.map('mapid').setView([51, 7], 13);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'balbla',
+        attribution: 'OSM Contributors &amp; <a href="github.com/webwurst">Tobias Bradtke aka webwurst</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoianNoYiIsImEiOiJjaW5nbW12M2kwMDA2dzdrbGVoNTNhaGsxIn0.DCS2ApHL4A5RSVvqnFPZ8Q'
